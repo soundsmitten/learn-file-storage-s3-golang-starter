c2dda8d6df2727a958f9c8069e2084da399abeba
Nicholas Lash <njlash@fastmail.fm>
2025-07-14 22:49:00 -0500

    ch1

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -33,5 +38,78 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20 // 10 MB
+
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse request", err)
+		return
+	}
+
+	mpfile, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get thumbnail from request", err)
+		return
+	}
+	contentType := header.Header.Get("Content-Type")
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Can't find video info", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "can't get media type", err)
+		return
+	}
+
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "invalid file", nil)
+		return
+	}
+
+	exts, err := mime.ExtensionsByType(contentType) 
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't get ext", err)
+		return
+	}
+
+	ext := ""
+	if exts != nil {
+		ext = exts[0]
+	} else {
+		log.Println("Unknown extension!")
+	}
+	filename := fmt.Sprintf("%v.%v", videoID.String(), ext)
+	path := filepath.Join(cfg.assetsRoot, filename)
+
+	file, err := os.Create(path)
+	defer file.Close()
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "can't create file", err)
+		return
+	}
+	if _, err := io.Copy(file, mpfile); err != nil {
+		log.Println("file write err")
+		respondWithError(w, http.StatusInternalServerError, "can't copy file", err)
+		return
+	}
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:8091/assets/%v", filename)
+	video.ThumbnailURL = &thumbnailUrl
+
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "can't save video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
